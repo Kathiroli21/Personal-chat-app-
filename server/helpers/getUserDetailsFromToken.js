983869e2f5543700f5b2ac0d2bfca38132d6a3c5
kathiroli21 <kathiroli216@gmail.com>
2024-08-05 15:18:35 +0530

    initial commit

---
+++ server/helpers/getUserDetailsFromToken.js
@@ -0,0 +1,31 @@
+const jwt = require('jsonwebtoken');
+const UserModel = require('../models/UserModel');
+
+const getUserDetailsFromToken = async (token) => {
+    if (!token) {
+        return {
+            message: "session out",
+            logout: true,
+        };
+    }
+
+    try {
+        const decode = await jwt.verify(token, process.env.JWT_SECREAT_KEY);
+        const user = await UserModel.findById(decode.id).select('-password');
+        return user;
+    } catch (error) {
+        if (error.name === 'TokenExpiredError') {
+            return {
+                message: "Token expired. Please log in again.",
+                logout: true,
+            };
+        } else {
+            return {
+                message: "Invalid token.",
+                logout: true,
+            };
+        }
+    }
+};
+
+module.exports = getUserDetailsFromToken;
